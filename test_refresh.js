e100783e997cface7b20742d5271c1e0a7bce064
Mario Salazar <152939674+gitmariosalazar@users.noreply.github.com>
2026-07-22 10:03:24 -0500

    feat: upload new changes

--- test_refresh.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-
-const data = JSON.stringify({
-  username_or_email: "TERAN TERAN", 
-  password: "password123" // Just a guess, or I don't know the password.
-});
-
-// Actually I don't know a valid password. Let me just search the logs to see what error the backend threw, if any.
